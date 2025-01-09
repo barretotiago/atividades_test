c0c80ae8f757af264960167e09fa5886ffa1566e
Tiago Barreto <105989038+barretotiago@users.noreply.github.com>
2025-01-09 19:00:53 -0300

    Adicionando arquivos

---
+++ Projetos Front/main.js
@@ -0,0 +1,19 @@
+const Pessoa = {
+    nome: 'Tiago',
+    idade: 28
+}
+
+document.body.innerHTML += ` <h1>O seu nome é ${Pessoa.nome} </h1>`
+document.body.innerHTML += ` <h1>A sua idade é ${Pessoa.idade} </h1>`
+
+
+
+
+
+
+
+//pegar o nome, idade, altura e peso do usuário
+// calcular o IMC
+// jogar todas as informações na tela com o innerHTML
+
+
